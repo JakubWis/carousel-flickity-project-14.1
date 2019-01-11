ad81313d41db140b2a67fd6d4fea8c25e73a6772
Jakub Wiśniewski <mrkubasny@gmail.com>
2019-01-11 14:09:31 +0100

    v 0.05

--- js/script.js
+++ js/script.js
@@ -2,20 +2,32 @@ var elem = document.querySelector('.main-carousel');
 var btnRestart = document.getElementById('restart');
 var progressBar = document.querySelector('.progress-bar')
 var templateCell = document.getElementById('template-cell').innerHTML;
+
 (function(){ 
-console.log(templateCell)
 Mustache.parse(templateCell);
 var cells = '';
 
 for(var i = 0; i < allSlides.length; i++){
-        console.log(allSlides[i]);
         cells += Mustache.render(templateCell, allSlides[i]);
-        console.log(cells)
     }
 
  results.insertAdjacentHTML('beforeend', cells);
 
-})(); 
+})();
+
+// Initialize and add the map
+window.initMap = function() {
+  // The location of Uluru
+  var uluru = allSlides[0].coords
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: uluru});
+  // The marker, positioned at Uluru
+  for (var i = 0; i < allSlides.length; i++) {
+    var marker = new google.maps.Marker({position: allSlides[i].coords, map: map});
+  }
+  
+} 
 
 var flkty = new Flickity( elem, {
   // options
